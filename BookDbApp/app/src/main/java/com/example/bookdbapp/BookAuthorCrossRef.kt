19943ca23b033bd039efd2a689d990cd521ebba9
David Černý <david.cerny.021@pslib.cz>
2025-01-11 18:00:44 +0100

    do-over

--- BookDbApp/app/src/main/java/com/example/bookdbapp/BookAuthorCrossRef.kt
+++
@@ -1,9 +0,0 @@
-package com.example.bookdbapp
-
-import androidx.room.Entity
-
-@Entity(primaryKeys = ["bookId", "authorId"])
-data class BookAuthorCrossRef(
-    val bookId: Long,
-    val authorId: Long
-)
